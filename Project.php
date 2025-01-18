e36a004e6d28c1c943af3474b3bcd1f560d19414
W0lf0x1 <131454305+W0lf0x1@users.noreply.github.com>
2025-01-18 11:19:52 +0500

    FirstProject.

--- Project.php
+++ Project.php
@@ -1,1 +1,26 @@
+<?php
 
+class Project {
+    private $name;
+    private $tasks;
+
+    public function __construct($name) {
+        $this->name = $name;
+        $this->tasks = [];
+        echo "Проект '{$this->name}' создан.\n";
+    }
+
+    public function __destruct() {
+        echo "Проект '{$this->name}' завершен.\n";
+    }
+
+    public function addTask($task) {
+        $this->tasks[] = $task;
+        echo "Задача '{$task->getTitle()}' добавлена в проект '{$this->name}'.\n";
+    }
+
+    public function __toString() {
+        $taskCount = count($this->tasks);
+        return "Проект: {$this->name}, Количество задач: {$taskCount}\n";
+    }
+}
