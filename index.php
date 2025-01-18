6dc69cb99f2008d5bec35d1e40b3fb2a6ccc320f
W0lf0x1 <131454305+W0lf0x1@users.noreply.github.com>
2025-01-18 11:20:49 +0500

    FirstProject.

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
 
+include 'Task.php';
+include 'Project.php';
+
+$project = new Project("Разработка нового сайта");
+
+$task1 = new Task("Создать дизайн", "Разработать макет главной страницы.");
+$task2 = new Task("Написать код", "Реализовать функционал сайта.");
+
+$project->addTask($task1);
+$project->addTask($task2);
+
+$task1->setStatus("В процессе");
+$task2->setStatus("Выполнено");
+
+echo $task1;
+echo $task2;
+
+echo $project;
+
+unset($task1);
+unset($task2);
+unset($project);
